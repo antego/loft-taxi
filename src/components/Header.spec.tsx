17443c0aa7dd3a94467103ad1ff6819b3921b633
antego <antego@users.noreply.github.com>
2021-01-23 21:56:00 +1100

    format

--- src/components/Header.spec.tsx
+++ src/components/Header.spec.tsx
@@ -1,14 +1,17 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Header from './Header';
+import { render } from "@testing-library/react";
+import React from "react";
+import Header from "./Header";
 
+const header = () => (
+  <Header onOrder={jest.fn()} onSignOut={jest.fn()} onProfile={jest.fn()} />
+);
 
 describe("Header", () => {
-    it("renders correctly", () => {
-        const { container } = render(<Header onOrder={jest.fn()} onSignOut={jest.fn()} onProfile={jest.fn()} />);
+  it("renders correctly", () => {
+    const { container } = render(header());
 
-        expect(container.innerHTML).toMatch("Карта")
-        expect(container.innerHTML).toMatch("Профиль")
-        expect(container.innerHTML).toMatch("Выйти")
-    })
-})
+    expect(container.innerHTML).toMatch("Карта");
+    expect(container.innerHTML).toMatch("Профиль");
+    expect(container.innerHTML).toMatch("Выйти");
+  });
+});
